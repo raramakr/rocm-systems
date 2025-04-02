034ffb2ef8466e5e10671978fabc45f80403204e
Marko Arandjelovic <Marko.Arandjelovic@amd.com>
2025-04-02 18:46:12 +0000

    SWDEV-519340 - Enable and fix hipModuleLoad test

--- projects/hip-tests/catch/unit/module/hipExtModuleLaunchKernel.cc
+++ projects/hip-tests/catch/unit/module/hipExtModuleLaunchKernel.cc
@@ -257,8 +257,9 @@ TEST_CASE("Unit_hipExtModuleLaunchKernel_Positive_Parameters") {
   SECTION("Pass only start event") {
     hipEvent_t start_event = nullptr;
     HIP_CHECK(hipEventCreate(&start_event));
-    const auto kernel = GetKernel(mg.module(), "NOPKernel");
-    HIP_CHECK(hipExtModuleLaunchKernel(kernel, 1, 1, 1, 1, 1, 1, 0, nullptr, nullptr, nullptr,
+    const auto kernel = GetKernel(GetModule(), "NOPKernel");
+    HIP_CHECK(hipExtModuleLaunchKernel(kernel, 1, 1, 1, 1, 1, 1, 0, nullptr,
+                                       nullptr, nullptr,
                                        start_event, nullptr));
     HIP_CHECK(hipDeviceSynchronize());
     HIP_CHECK(hipEventQuery(start_event));
@@ -268,8 +269,9 @@ TEST_CASE("Unit_hipExtModuleLaunchKernel_Positive_Parameters") {
   SECTION("Pass only stop event") {
     hipEvent_t stop_event = nullptr;
     HIP_CHECK(hipEventCreate(&stop_event));
-    const auto kernel = GetKernel(mg.module(), "NOPKernel");
-    HIP_CHECK(hipExtModuleLaunchKernel(kernel, 1, 1, 1, 1, 1, 1, 0, nullptr, nullptr, nullptr,
+    const auto kernel = GetKernel(GetModule(), "NOPKernel");
+    HIP_CHECK(hipExtModuleLaunchKernel(kernel, 1, 1, 1, 1, 1, 1, 0, nullptr,
+                                       nullptr, nullptr,
                                        nullptr, stop_event));
     HIP_CHECK(hipDeviceSynchronize());
     HIP_CHECK(hipEventQuery(stop_event));
