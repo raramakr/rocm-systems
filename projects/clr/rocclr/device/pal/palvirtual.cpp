da7db537072f52cab4170b822893ed43cd8d7824
Laurent Morichetti <laurent.morichetti@amd.com>
2025-08-01 01:01:58 -0700

    clr: SWDEV-547890 - Conservatively update the MQD's read_dispatch_id
    The read_dispatch_id cannot be smaller than the current aql_packet_id
    - hsa_queue.size for the debugger to work correctly.
    
    The read_dispatch_id really should be updated when the CmdBuf is marked
    as complete. Left a FIXME to address it in a future commit.

--- projects/clr/rocclr/device/pal/palvirtual.cpp
+++ projects/clr/rocclr/device/pal/palvirtual.cpp
@@ -2773,6 +2773,31 @@ bool VirtualGPU::submitKernelInternal(const amd::NDRangeContainer& sizes, const 
   amd_queue.scratch_backing_memory_location = static_cast<uint64_t>(dispatchParam.scratchAddr);
   amd_queue.scratch_backing_memory_byte_size = static_cast<uint64_t>(dispatchParam.scratchSize);
 
+  // FIXME: Conservatively, the read_dispatch_id cannot be smaller than the current aql_packet_id -
+  // hsa_queue.size for the debugger to work correctly. The read_dispatch_id really should be
+  // updated when the CmdBuf is marked as complete.
+  uint64_t new_read_dispatch_id = (aql_packet_id >= amd_queue.hsa_queue.size)
+      ? (aql_packet_id - amd_queue.hsa_queue.size + 1)
+      : 0;
+
+  // Do an atomic max of &amd_queue.read_dispatch_id and new_read_dispatch_id
+  uint64_t old_read_dispatch_id = amd_queue.read_dispatch_id;
+  while (new_read_dispatch_id > old_read_dispatch_id) {
+#if defined(__GNUC__)
+    if (__atomic_compare_exchange_n(&amd_queue.read_dispatch_id, &old_read_dispatch_id,
+                                    new_read_dispatch_id, true, __ATOMIC_RELAXED, __ATOMIC_RELAXED))
+      break;
+#elif defined(_MSC_VER)
+    uint64_t initial_value = InterlockedCompareExchange64(
+        reinterpret_cast<LONG64 volatile*>(&amd_queue.read_dispatch_id), new_read_dispatch_id,
+        old_read_dispatch_id);
+    if (initial_value == old_read_dispatch_id) break;
+    old_read_dispatch_id = initial_value;
+#else  // !defined (_MSV_VER) && !defined(__GNUC__)
+#error Not implemented
+#endif  // !defined (_MSV_VER) && !defined(__GNUC__)
+  }
+
   // Run AQL dispatch in HW
   eventBegin(MainEngine);
 
