390068df7277892a63e3f806eb0dca71af2c00c4
Anavena Venkatesh <Anavena.Venkatesh@amd.com>
2025-09-30 10:34:35 +0000

    SWDEV-533237 : Added test cases for hipOccupancyAvailableDynamicSMemPerBlock

--- projects/hip-tests/catch/unit/occupancy/hipOccupancyAvailableDynamicSMemPerBlock.cc
+++ projects/hip-tests/catch/unit/occupancy/hipOccupancyAvailableDynamicSMemPerBlock.cc
@@ -18,14 +18,16 @@ THE SOFTWARE.
 */
 
 /**
- * @brief Returns dynamic shared memory available per block when launching numBlocks blocks on SM.
+ * @brief Returns dynamic shared memory available per block when launching
+ * numBlocks blocks on SM.
  *
  * @ingroup Occupancy
  * Returns in dynamicSmemSize the maximum size of dynamic shared memory
  * to allow numBlocks blocks per SM.
  *
  * @param [out] dynamicSmemSize Returned maximum dynamic shared memory.
- * @param [in]  f               Kernel function for which occupancy is calculated.
+ * @param [in]  f               Kernel function for which occupancy is
+ * calculated.
  * @param [in]  numBlocks       Number of blocks to fit on SM
  * @param [in]  blockSize       Size of the block
  *
@@ -39,7 +41,7 @@ THE SOFTWARE.
 
 const size_t LEN = 1024;
 
-__global__ void dynamicReverse(int* d, int n) {
+__global__ void dynamicReverse(int *d, int n) {
   __shared__ int s[LEN];
 
   int t = threadIdx.x;
@@ -49,7 +51,8 @@ __global__ void dynamicReverse(int* d, int n) {
   __syncthreads();
   d[t] = s[tr];
 }
-static __global__ void f1(float* a) { *a = 1.0; }
+
+static __global__ void f1(float *a) { *a = 1.0; }
 
 /**
  * Test Description
@@ -68,40 +71,62 @@ TEST_CASE("Unit_hipOccupancyAvailableDynamicSMemPerBlock_Negative") {
   int numBlocks = 1;
   int blockSize = 1024;
   SECTION("Invalid driver funtion") {
-    HIP_CHECK_ERROR(
-        hipOccupancyAvailableDynamicSMemPerBlock(&dynamicSmemSize, NULL, numBlocks, blockSize),
-        hipErrorInvalidDeviceFunction);
+    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(
+                        &dynamicSmemSize, NULL, numBlocks, blockSize),
+                    hipErrorInvalidDeviceFunction);
   }
 
   SECTION("Number of blocks are zero") {
-    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(&dynamicSmemSize, f1, 0, blockSize),
+    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(&dynamicSmemSize,
+                                                             f1, 0, blockSize),
                     hipErrorInvalidValue);
   }
 
   SECTION("Block size is zero") {
-    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(&dynamicSmemSize, f1, numBlocks, 0),
+    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(&dynamicSmemSize,
+                                                             f1, numBlocks, 0),
                     hipErrorInvalidValue);
   }
 
   SECTION("dynamicSmemSize is null") {
-    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(nullptr, f1, numBlocks, blockSize),
+    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(
+                        nullptr, f1, numBlocks, blockSize),
                     hipErrorInvalidValue);
   }
+
   SECTION("Threads limitation exceded ") {
     hipDeviceProp_t devProp;
     HIP_CHECK(hipGetDeviceProperties(&devProp, 0));
     blockSize = devProp.maxThreadsPerMultiProcessor + 1;
-    HIP_CHECK_ERROR(
-        hipOccupancyAvailableDynamicSMemPerBlock(&dynamicSmemSize, f1, numBlocks, blockSize),
-        hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(
+                        &dynamicSmemSize, f1, numBlocks, blockSize),
+                    hipErrorInvalidValue);
+  }
+
+  SECTION("maxSharedMemoryPerMultiProcessor limit exceded ") {
+    hipDeviceProp_t devProp;
+    numBlocks = devProp.maxSharedMemoryPerMultiProcessor;
+    HIP_CHECK(hipGetDeviceProperties(&devProp, 0));
+    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(
+                        &dynamicSmemSize, f1, numBlocks, blockSize),
+                    hipErrorInvalidValue);
+  }
+
+  SECTION("regsPerMultiprocessor limit exceded") {
+    hipDeviceProp_t devProp;
+    numBlocks = devProp.regsPerMultiprocessor;
+    HIP_CHECK(hipGetDeviceProperties(&devProp, 0));
+    HIP_CHECK_ERROR(hipOccupancyAvailableDynamicSMemPerBlock(
+                        &dynamicSmemSize, f1, numBlocks, blockSize),
+                    hipErrorInvalidValue);
   }
 }
 
 /**
  * Test Description
  * ------------------------
- *  - This test will verify funtionality of hipOccupancyAvailableDynamicSMemPerBlock api
- * Test source
+ *  - This test will verify funtionality of
+ * hipOccupancyAvailableDynamicSMemPerBlock api Test source
  * ------------------------
  * - occupancy/hipOccupancyAvailableDynamicSMemPerBlock.cc
  * Test requirements
@@ -109,7 +134,7 @@ TEST_CASE("Unit_hipOccupancyAvailableDynamicSMemPerBlock_Negative") {
  * - HIP_VERSION >= 7.1
  */
 TEST_CASE("Unit_hipOccupancyAvailableDynamicSMemPerBlock_Positive") {
-  size_t dynamicSmemSize;
+  size_t dynamicSmemSize = 0;
   int numBlocks = 1;
   int blockSize = 1024;
   int a[LEN], r[LEN], d[LEN];
@@ -120,17 +145,15 @@ TEST_CASE("Unit_hipOccupancyAvailableDynamicSMemPerBlock_Positive") {
     d[i] = 0;
   }
 
-  int* d_d;                                       // Device pointer
+  int *d_d;                                      // Device pointer
   HIP_CHECK(hipMalloc(&d_d, LEN * sizeof(int)));  // Allocate device memory
 
   HIP_CHECK(hipMemcpy(d_d, a, LEN * sizeof(int), hipMemcpyHostToDevice));
 
-  HIP_CHECK(hipOccupancyAvailableDynamicSMemPerBlock(&dynamicSmemSize, dynamicReverse, numBlocks,
-                                                     blockSize));
-
+  HIP_CHECK(hipOccupancyAvailableDynamicSMemPerBlock(
+      &dynamicSmemSize, dynamicReverse, numBlocks, blockSize));
   hipDeviceProp_t devProp;
   HIP_CHECK(hipGetDeviceProperties(&devProp, 0));
-
   REQUIRE(dynamicSmemSize == devProp.sharedMemPerBlock - LEN * sizeof(int));
   dynamicReverse<<<1, LEN, LEN * sizeof(int)>>>(d_d, LEN);
 
