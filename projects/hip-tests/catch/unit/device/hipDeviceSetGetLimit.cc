1283f34e4bdc26e8949ddc21f8432062793a4ca7
Aleksandar Milanov <Aleksandar.Milanov@amd.com>
2025-10-09 01:18:26 +0200

    [hip-tests] Tag multigpu tests with Catch2 tags

--- projects/hip-tests/catch/unit/device/hipDeviceSetGetLimit.cc
+++ projects/hip-tests/catch/unit/device/hipDeviceSetGetLimit.cc
@@ -454,7 +454,7 @@ void getMinMaxCurrentAndSetCurrent() {
  * ------------------------
  *  - HIP_VERSION >= 6.5
  */
-TEST_CASE("Unit_hipDeviceGetSetLimit_Scratch_MultiDevice") {
+TEST_CASE("Unit_hipDeviceGetSetLimit_Scratch_MultiDevice", "[multigpu]") {
   int deviceCount = 0;
   HIP_CHECK(hipGetDeviceCount(&deviceCount));
   if (deviceCount < 2) {
