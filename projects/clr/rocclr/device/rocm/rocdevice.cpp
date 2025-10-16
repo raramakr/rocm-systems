dcdfb76e2db4e76db94012b4d522197c9384d426
Sam Ruscica <sam.ruscica@amd.com>
2025-10-16 15:56:33 -0400

    Updated amdFileRead in rocdevice to support windows build

--- projects/clr/rocclr/device/rocm/rocdevice.cpp
+++ projects/clr/rocclr/device/rocm/rocdevice.cpp
@@ -1725,15 +1725,16 @@ bool Device::globalFreeMemory(size_t* freeMemory) const {
 bool Device::amdFileRead(amd::Os::FileDesc handle, void* devicePtr, uint64_t size, int64_t file_offset,
                       uint64_t* size_copied, int32_t* status) {
   hsa_amd_ais_file_handle_t fh{};
+  hsa_status_t ret = HSA_STATUS_ERROR;
 #if defined(_WIN32)
   fh.handle = handle;
 #else
   fh.fd = handle;
+  ret = Hsa::ais_file_read(fh, devicePtr, size, file_offset, size_copied, status);
 #endif
-  hsa_status_t ret = Hsa::ais_file_read(fh,
-                                        devicePtr, size, file_offset, size_copied, status);
+
   if (HSA_STATUS_SUCCESS != ret) {
-    LogPrintfError("hsa_amd_ais_file_read operation failed with err 0x%xh", ret);
+    LogPrintfError("amdFileRead operation failed with err 0x%xh", ret);
     return false;
   }
   return true;
