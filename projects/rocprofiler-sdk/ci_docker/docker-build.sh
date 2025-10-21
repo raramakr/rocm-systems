f98296529ddd5ef16ed907a88cb69108cc7b591d
Ammar ELWazir <aelwazir@amd.com>
2025-10-21 15:44:58 -0500

    Fixing Docker CI files

--- projects/rocprofiler-sdk/ci_docker/docker-build.sh
+++ projects/rocprofiler-sdk/ci_docker/docker-build.sh
@@ -21,7 +21,7 @@ build_stage1_image() {
 
     echo "Building stage-1 (deps) ${os_name}-${os_version} image..."
 
-    docker build --no-cache \
+    docker build \
         -f "${SCRIPT_DIR}/${dockerfile}" \
         -t "${REGISTRY}/${BASE_TAG}:${os_name}-${os_version}-${BUILD_DATE}" \
         -t "${REGISTRY}/${BASE_TAG}:${os_name}-${os_version}-latest-testing" \
@@ -53,7 +53,7 @@ build_stage2_image() {
 
     echo "Building stage-2 (final) ${os_tag}-${gpu} using ${TARBALL_KEYS["${gpu}"]}..."
 
-    docker build --no-cache \
+    docker build \
         -f "${SCRIPT_DIR}/Dockerfile.stages" \
         --build-arg BASE_IMAGE="${REGISTRY}/${BASE_TAG}:${os_tag}-latest-testing" \
         --build-arg GPU_TYPE="${gpu}" \
